e7f211a23860862c3fb412fc398e00f1642f08cb
celiarodriguezmo <celiro02@gmail.com>
2022-04-08 12:28:00 +0200

    change event listener to handle the text of input search when the user click on button search

--- src/js/0_ search cocktails.js
+++ src/js/0_ search cocktails.js
@@ -2,62 +2,40 @@
 
 const inputSearch = document.querySelector(".js-input-search");
 const btnSearch = document.querySelector(".js-btn-search");
+const paintList = document.querySelector(".js-list");
 const urlApiCocktails =
-  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-//al iniciar la página ya debo tener acceso al servidor con los datos de los cocteles
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 let infoCocktailsServer = [];
-
-fetch(urlApiCocktails)
-  .then((response) => response.json())
-  .then((serverInfo) => {
-    infoCocktailsServer = serverInfo.drinks;
-
-    for (const infoOneCocktail of infoCocktailsServer) {
-      let ulContent = infoOneCocktail;
-      console.log(ulContent.strDrink);
-      paintList.innerHTML += `<li>
-      <article>
-       <h2>${ulContent.strDrink}</h2>
-      <img
-          src="${ulContent.strDrinkThumb}"
-          alt="cocktail Margarita por defecto"
-       />
-     </article>
-    </li>`;
-    }
-  });
-
-//Tengo que hacer un evento para escuchar cuando la usuaria teclea en el input de texto y recoger el valor de lo que escribe
-
-function handleUserCocktailSearchForm(event) {
-  let textSearchByUser = event.target.value;
-  console.log(event.target.value);
-}
-
-inputSearch.addEventListener("keyup", handleUserCocktailSearchForm);
-
-// tengo que escuchar el evento en el botón buscar del formulario, luego comparar el valor de lo que ha escrito la usuaria con la base de datos.
+let liContent = [];
+let textSearchByUser = "";
 
 function handleUserSearchForm() {
-  console.log("Hey quieres buscar algún cóctel¿?!");
-  paintListOfCocktails();
+  textSearchByUser = inputSearch.value;
+  fetch(`${urlApiCocktails}${textSearchByUser}`)
+    .then((response) => response.json())
+    .then((serverInfo) => {
+      infoCocktailsServer = serverInfo.drinks;
+      paintList.innerHTML = "";
+      for (const infoOneCocktail of infoCocktailsServer) {
+        let liContent = infoOneCocktail;
+        paintCockteles(liContent);
+      }
+    });
+}
+
+//esta función pinta cada elemento de la lista de Cockteles
+
+function paintCockteles(liContent) {
+  paintList.innerHTML += `<li>
+        <article>
+         <h2>${liContent.strDrink}</h2>
+        <img
+            src="${liContent.strDrinkThumb}"
+            alt="cocktail Margarita por defecto"
+         />
+       </article>
+      </li>`;
 }
 
 btnSearch.addEventListener("click", handleUserSearchForm);
-
-//Luego tengo que pintar la lista de los Cócteles que coincidan
-
-const paintList = document.querySelector(".js-list");
-
-function paintListOfCocktails() {
-  paintList.innerHTML += `<li>
-    <article>
-      <h2>${drinks.strDrink}</h2>
-      <img
-        src="./assets/images/margarita.jpg"
-        alt="cocktail Margarita por defecto"
-      />
-    </article>
-  </li>`;
-}
